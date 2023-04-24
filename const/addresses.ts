af542d068c8c54a551f497b0bc6209528f7c8d3d
Byoungcheon Lee <lbcsultan@gmail.com>
2023-04-24 16:22:07 +0900

    '1st'

---
+++ const/addresses.ts
@@ -0,0 +1,8 @@
+// Wallet address : 0xE80A51ab33cAefE128AC9AE04BF6822C8FBb619b
+// Contract name : HotMarket, Marketplace-v3
+// NFT collection name : NFT collection test by sultan
+
+export const MARKETPLACE_ADDRESS = '0x17b5d3C0B46EE13Cba246041F48856ed3841726e'
+
+export const NFT_COLLECTION_ADDRESS =
+  '0x4b37532Eb5e2f40BF31e34d90A52454FdB003128'
---
+++ pages/buy.tsx
@@ -0,0 +1,16 @@
+import { Container, Heading, Text } from '@chakra-ui/react'
+import NFTGrid from '../components/NFTGrid'
+import { NFT_COLLECTION_ADDRESS } from '../const/addresses'
+import { useContract, useNFTs } from '@thirdweb-dev/react'
+
+export default function Buy() {
+  const { contract } = useContract(NFT_COLLECTION_ADDRESS)
+  const { data, isLoading } = useNFTs(contract)
+  return (
+    <Container maxW={'1200px'} p={6}>
+      <Heading>Buy NFTs</Heading>
+      <Text>Browse and buy NFTs from this collection.</Text>
+      <NFTGrid isLoading={isLoading} data={data} emptyText={'No NFTs Found'} />
+    </Container>
+  )
+}
